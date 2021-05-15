e63d2c33ae4e6cd3314499ef5c44f04b75b96c88
gvtek0 <gvtek0@gmail.com>
2021-05-15 13:00:02 -0700

    comment edits

--- main.tf
+++ main.tf
@@ -1,27 +1,27 @@
-# Create the VPC
+# Create VPC
 resource "aws_vpc" "Main" {
     cidr_block = var.main_vpc_cidr
     instance_tenancy = "default"
 }
 
-# Create Internet gateway
+# Create IGW
 resource "aws_internet_gateway" "IGW" {
   vpc_id = aws_vpc.Main.id
 }
 
-# Create Public subnets
+# Create public
 resource "aws_subnet" "publicsubnets" {
   vpc_id = aw_vpc.Main.id
   cidr_block = "${var.public_subnets}"
 }
 
-# Create private subnets
+# Create private
 resource "aws_subnet" "privatesubnets" {
   vpc_id = aws_vpc.Main.id
   cidr_block = "${var.private_subnets}"
 }
 
-# Route table for Public subnets)
+# Route table for public
 resource "aws_route_table" "PublicRT" {
   vpc_id = aws_vpc.Main.id
     route {
