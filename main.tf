1a8b04e52fc7b242558a3a2d4af3b3dd9a5803d0
gvtek0 <gvtek0@gmail.com>
2021-05-15 12:58:49 -0700

    comments per blocks and add route table for public subnets

--- main.tf
+++ main.tf
@@ -1,18 +1,31 @@
+# Create the VPC
 resource "aws_vpc" "Main" {
     cidr_block = var.main_vpc_cidr
     instance_tenancy = "default"
 }
 
+# Create Internet gateway
 resource "aws_internet_gateway" "IGW" {
   vpc_id = aws_vpc.Main.id
 }
 
+# Create Public subnets
 resource "aws_subnet" "publicsubnets" {
   vpc_id = aw_vpc.Main.id
   cidr_block = "${var.public_subnets}"
 }
 
+# Create private subnets
 resource "aws_subnet" "privatesubnets" {
   vpc_id = aws_vpc.Main.id
   cidr_block = "${var.private_subnets}"
+}
+
+# Route table for Public subnets)
+resource "aws_route_table" "PublicRT" {
+  vpc_id = aws_vpc.Main.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id aws_internet_gateway.IGW.id
+    }
 }
