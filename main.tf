fed41d67a04dfbd331e576f694cbdd21ad76b692
gvtek0 <gvtek0@gmail.com>
2021-05-15 12:54:56 -0700

    add IGW and pub/priv subnets

--- main.tf
+++ main.tf
@@ -2,3 +2,17 @@ resource "aws_vpc" "Main" {
     cidr_block = var.main_vpc_cidr
     instance_tenancy = "default"
 }
+
+resource "aws_internet_gateway" "IGW" {
+  vpc_id = aws_vpc.Main.id
+}
+
+resource "aws_subnet" "publicsubnets" {
+  vpc_id = aw_vpc.Main.id
+  cidr_block = "${var.public_subnets}"
+}
+
+resource "aws_subnet" "privatesubnets" {
+  vpc_id = aws_vpc.Main.id
+  cidr_block = "${var.private_subnets}"
+}
